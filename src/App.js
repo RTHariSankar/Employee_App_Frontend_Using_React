a26b44de93ffda1d16358bdd2584c8de05e2e887
HariSankarRT <masterchess117@gmail.com>
2023-07-08 19:25:13 +0530

    5544332

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
     <div>
       <BrowserRouter basename='/employee_app_frontend_using_react'>
         <Routes basename='/employee_app_frontend_using_react'>
-          <Route basename='/employee_app_frontend_using_react' path='/' element={<Home/>}/>
-          <Route basename='/employee_app_frontend_using_react' path='/employee_form' element={<Employeeform/>}/>
+          <Route basename='/employee_app_frontend_using_react' path='/employee_app_frontend_using_react' element={<Home/>}/>
+          <Route basename='/employee_app_frontend_using_react' path='/employee_app_frontend_using_react' element={<Employeeform/>}/>
         </Routes>
       </BrowserRouter>
     </div>
