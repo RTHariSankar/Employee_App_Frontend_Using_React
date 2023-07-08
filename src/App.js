1a3f599550cf99dfc23778d3400efa1b58b7f494
HariSankarRT <masterchess117@gmail.com>
2023-07-08 19:00:00 +0530

    123132132

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './components/Home';
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename='/employee_app_frontend_using_react'>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/employee_form' element={<Employeeform/>}/>
